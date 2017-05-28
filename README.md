dba2d6bca1d9cb694c28a4f273aa667d8908315e
Eugen Soloviov <suenot@gmail.com>
2017-05-28 17:00:22 +0300

    nuxtjs: starter kit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# frontend
+# open-growbox frontend
 
+### To start
+install the dependencies:
+```sh
+cd <project-name>
+npm install
+```
+and launch the project with:
+```sh
+npm run dev
+```
+The application is now running on http://localhost:3000
+
+### Thanks to
 E.g.: Icon made by [Freepik](http://www.freepik.com/) from www.flaticon.com
---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        NUXT about
+      </h1>
+      <h2 class="subtitle">
+        Universal Vue.js Application
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">Github</a>
+      </div>
+    </div>
+  </section>
+</template>
